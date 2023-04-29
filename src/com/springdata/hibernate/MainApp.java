33aadb6e215c5b3b1f20b096604e3662bb15c059
Gemy98 <mohamedgamal19981998@gmail.com>
2023-04-29 08:23:22 +0300

    save object client

--- src/com/springdata/hibernate/MainApp.java
+++ src/com/springdata/hibernate/MainApp.java
@@ -3,11 +3,13 @@ package com.springdata.hibernate;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Random;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.mysql.cj.protocol.x.Notice.XSessionVariableChanged;
 import com.springdata.hibernate.model.Client;
 
 public class MainApp {
@@ -21,7 +23,29 @@ public class MainApp {
 				.buildSessionFactory();		
 		 Session session = factory.getCurrentSession();
 		
+		 
+	Client client = new Client();
+	client.setId((long)1);
+	client.setAge(20);
+	client.setAddress("Cairo");
+	client.setFullName("MohamedGamal");
+	
+	
+	try {
+		session.beginTransaction();
+		session.save(client);
+		session.getTransaction().commit();
 		
+		System.out.println("commit Success");
+		
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+	//	e.printStackTrace();
+		System.out.println("Failed to save this client");
+	}
+	finally {
+		session.close();
+	}
 		
 	}
 

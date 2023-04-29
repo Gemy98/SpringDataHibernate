939c13607685ec7b301c077bfd89448339645d84
Gemy98 <mohamedgamal19981998@gmail.com>
2023-04-29 04:52:51 +0300

    Create Session and SessionFactory

--- src/com/springdata/hibernate/MainApp.java
+++ src/com/springdata/hibernate/MainApp.java
@@ -4,13 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.springdata.hibernate.model.Client;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Client.class)
+				.buildSessionFactory();		
+		 Session session = factory.getCurrentSession();
 		
 		
 		

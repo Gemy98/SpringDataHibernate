8f4bfe42805e49b7b355782629ca88e9e2c287a6
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-06 01:27:45 +0300

    Cascade type Merge

--- src/com/springdata/hibernate/model/Person.java
+++ src/com/springdata/hibernate/model/Person.java
@@ -15,7 +15,7 @@ import javax.persistence.Table;
 public class Person {
 	
 	
-	@OneToOne(cascade = CascadeType.REMOVE)
+	@OneToOne(cascade = CascadeType.MERGE)
 	@JoinColumn(name ="id_data")
 	Data data = new Data();
 	

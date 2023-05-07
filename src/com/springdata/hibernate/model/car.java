23b6584b2ca3ed1ad5ed95ada3d919536d86df32
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-07 06:06:01 +0300

    Create Model Car - Color

---
+++ src/com/springdata/hibernate/model/car.java
@@ -0,0 +1,34 @@
+package com.springdata.hibernate.model;
+
+public class car {
+
+	private int id ;
+	
+	private String name ;
+	
+	
+	public car() {
+		
+		
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/com/springdata/hibernate/model/color.java
@@ -0,0 +1,33 @@
+package com.springdata.hibernate.model;
+
+
+
+public class color {
+	
+	
+	private int id ;
+	
+	private String name ;
+	
+	public color() {
+		
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}

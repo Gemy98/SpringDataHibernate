7ea0e24e31403e581b82ba949cc05201ab5277aa
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-08 23:24:59 +0300

    Relationship Car - Color / JoinTable - ManytoMany

--- src/com/springdata/hibernate/model/car.java
+++ src/com/springdata/hibernate/model/car.java
@@ -1,10 +1,18 @@
 package com.springdata.hibernate.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,7 +27,25 @@ public class Car {
 	@Column(name="name")
 	private String name ;
 	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(
+			name = "car_color",
+			joinColumns =@JoinColumn(name ="car_id"),
+			inverseJoinColumns =@JoinColumn(name ="color_id")
+			  )
+	List<Color> colors = new ArrayList<Color>();
 	
+	
+	public List<Color> getColors() {
+		return colors;
+	}
+
+
+	public void setColors(List<Color> colors) {
+		this.colors = colors;
+	}
+
+
 	public Car() {
 		
 		
